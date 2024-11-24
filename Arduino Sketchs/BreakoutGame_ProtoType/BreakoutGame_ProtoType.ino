5e9efc678fd79d2e2b14920dbbfaf6a5ccb88f19
LittleDragonGeek <132950307+LittleDragonGeek@users.noreply.github.com>
2024-11-24 22:46:47 +0100

    Created new branch dev/Eric and Arduino Sketchs/BreakoutGame_ProtoType/BreakoutGame_ProtoType.ino was edited

--- Arduino Sketchs/BreakoutGame_ProtoType/BreakoutGame_ProtoType.ino
+++ Arduino Sketchs/BreakoutGame_ProtoType/BreakoutGame_ProtoType.ino
@@ -1,13 +1,21 @@
+// Import von h-Dateien (Klassen)
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7735.h>
 #include <SPI.h>
 
+// Definiere Pins an ESP32/Arduino
 #define TFT_MOSI  23
 #define TFT_SCLK  18
 #define TFT_CS    19
 #define TFT_RST   21
 #define TFT_DC    22
 
+#define BUTTON_RIGHT  14  // A
+#define BUTTON_DOWN   15  // B
+#define BUTTON_UP     17  // X
+#define BUTTON_LEFT   16  // Y
+
+// Display: 128x160
 SPIClass tftSPI = SPIClass(HSPI);
 Adafruit_ST7735 tft = Adafruit_ST7735(&tftSPI, TFT_CS, TFT_DC, TFT_RST); // SPI, CS, DC, RST
 
@@ -20,27 +28,34 @@ void setup() {
   tft.setSPISpeed(40000000);
 }
 
-void testfillrects(uint16_t color1, uint16_t color2) {
+void setBricks(uint16_t color1) {
+  tft.setRotation(1);
   tft.fillScreen(ST77XX_WHITE);
-  tft.fillRect(40, 150, 30, 5, color2);
-  tft.fillRect(10, 50, 5, 5, color1);
-  tft.fillRect(20, 50, 5, 5, color1);
-  tft.fillRect(30, 50, 5, 5, color1);
-  tft.fillRect(40, 50, 5, 5, color1);
-  tft.fillRect(50, 50, 5, 5, color1);
-  tft.fillRect(60, 50, 5, 5, color1);
-  tft.fillRect(70, 50, 5, 5, color1);
-  tft.fillRect(80, 50, 5, 5, color1);
-  tft.fillRect(90, 50, 5, 5, color1);
-  tft.fillRect(100, 50, 5, 5, color1);
-  tft.fillRect(110, 50, 5, 5, color1);
+  tft.fillRect(5, 50, 10, 5, color1);
+  tft.fillRect(20, 50, 10, 5, color1);
+  tft.fillRect(35, 50, 10, 5, color1);
+  tft.fillRect(50, 50, 10, 5, color1);
+  tft.fillRect(65, 50, 10, 5, color1);
+  tft.fillRect(80, 50, 10, 5, color1);
+  tft.fillRect(95, 50, 10, 5, color1);
+  tft.fillRect(110, 50, 10, 5, color1);
+  tft.fillRect(125, 50, 10, 5, color1);
+  tft.fillRect(140, 50, 10, 5, color1);
+  tft.fillRect(110, 50, 10, 5, color1);
+}
+
+void setPlayer(uint16_t color2) {
+  tft.setRotation(1);
+  tft.fillRect(40, 115, 30, 5, color2);
 }
 
 void loop() {
-  testfillrects(ST77XX_BLUE, ST77XX_ORANGE);
-  int posY = 60 + 90*(millis()%1000)/1000.0f;
-  tft.fillCircle(60, posY, 3, ST77XX_BLACK);
-  tft.setCursor(15, 0);
+  setBricks(ST77XX_BLUE);
+  setPlayer(ST77XX_ORANGE);
+  int posY = 40 + 60*(millis()%1000)/1000.0f;
+  int posX = 40 + 60*(millis()%1000)/1000.0f;
+  tft.fillCircle(posX, posY, 3, ST77XX_BLACK);
+  tft.setCursor(30, 0);
   tft.setTextColor(ST77XX_BLACK);
   tft.setTextSize(1.5);
   tft.print("Breaking Out Inc.");
